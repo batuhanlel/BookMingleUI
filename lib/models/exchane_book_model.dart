31be9bd4b771f8275ef5bba087cfae73451d91f0
batuhanlel <lelbatuhan@gmail.com>
2023-04-10 23:41:22 +0300

    feat: exchange book api and model

---
+++ lib/models/exchane_book_model.dart
@@ -0,0 +1,59 @@
+class ExchangeBookResponseModel {
+  final int bookId;
+  final String title;
+  final String author;
+  final int userId;
+  final String userName;
+  final String userSurname;
+  final String userEmail;
+  final String error;
+  final Map<String, dynamic> errors;
+
+  ExchangeBookResponseModel(
+    this.bookId,
+    this.title,
+    this.author,
+    this.userId,
+    this.userName,
+    this.userSurname,
+    this.userEmail,
+    this.error,
+    this.errors,
+  );
+
+  factory ExchangeBookResponseModel.fromJson(Map<String, dynamic> json) {
+    return ExchangeBookResponseModel(
+        json["bookId"] ?? "",
+        json["title"] ?? "",
+        json["author"] ?? "",
+        json["userId"] ?? "",
+        json["userName"] ?? "",
+        json["userSurname"] ?? "",
+        json["userEmail"] ?? "",
+        json["error"] ?? "",
+        json["errors"] ?? {});
+  }
+}
+
+class ExchangeBookRequestModel {
+  String? searchText;
+  String? sort;
+  int page;
+  int? length;
+
+  ExchangeBookRequestModel({
+    this.searchText,
+    this.sort = 'title',
+    required this.page,
+    this.length = 1,
+  });
+
+  Map<String, dynamic> toQueryParam() {
+    return <String, dynamic>{
+      "searchText": searchText,
+      "sort": sort,
+      "page": page.toString(),
+      "length": length.toString(),
+    };
+  }
+}
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'package:book_mingle_ui/models/exchane_book_model.dart';
 import 'package:book_mingle_ui/models/login_model.dart';
 import 'package:book_mingle_ui/models/signup_model.dart';
 import 'package:book_mingle_ui/constant.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
@@ -37,6 +39,31 @@ class ApiService {
 
     if (response.statusCode == 200 || response.statusCode == 400) {
       return SignUpResponseModel.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception("Failed to Get Data");
+    }
+  }
+
+  static Future<List<ExchangeBookResponseModel>> exchangeBookRecommendation(ExchangeBookRequestModel requestModel) async {
+    const storage = FlutterSecureStorage();
+    final token = await storage.read(key: 'token');
+    String url = "$baseUrl/exchange/list/get";
+    final headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Accept' : '*/*',
+      'Authorization' : 'Bearer $token'
+    };
+
+    final response = await http.get(Uri.parse(url).replace(queryParameters: requestModel.toQueryParam()), headers: headers);
+
+    if (response.statusCode == 200) {
+      List<dynamic> jsonArray = jsonDecode(response.body);
+      List<ExchangeBookResponseModel> items = [];
+      for (var i = 0; i < jsonArray.length; i++) {
+        items.add(ExchangeBookResponseModel.fromJson(jsonArray[i]));
+      }
+      return items;
     } else {
       throw Exception("Failed to Get Data");
     }
